774ba455dacf9cacaa5c2616598a5cfc4d601666
Seth Hollyman <shollyman@google.com>
2023-03-27 23:13:36 +0000

    improve logging pattern

--- bigquery/storage/managedwriter/debug.go
+++ bigquery/storage/managedwriter/debug.go
@@ -16,6 +16,9 @@ package managedwriter
 
 import (
 	"context"
+	"fmt"
+	"time"
+	"unsafe"
 
 	"golang.org/x/exp/slog"
 	"google.golang.org/grpc"
@@ -23,50 +26,116 @@ import (
 )
 
 func DebugStreamLogger(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-	slog.Info("opening client stream",
-		slog.String("method", method))
+	dcs := &debugClientStream{
+		ctx:    ctx,
+		method: method,
+		id:     fmt.Sprintf("debug_stream_%d", time.Now().UnixNano()), // TODO: real uuid,
+	}
+	log := dcs.log(slog.LevelInfo, "intercepting ClientStream")
+	defer log.log()
 
 	real, err := streamer(ctx, desc, cc, method, opts...)
 	if err != nil {
-		slog.Info("failed to intercept client stream")
+		log.addAttr(slog.Any("Error", err))
 		return nil, err
 	}
-	return &debugClientStream{
-		real: real,
-	}, nil
-
+	dcs.real = real
+	return dcs, nil
 }
 
+const (
+	clientStreamMethodName = "ClientStreamMethod"
+)
+
 type debugClientStream struct {
-	real grpc.ClientStream
+	ctx    context.Context
+	real   grpc.ClientStream
+	id     string
+	method string
+}
+
+type logData struct {
+	ctx   context.Context
+	level slog.Level
+	msg   string
+	attrs []slog.Attr
+}
+
+func (ld *logData) log() {
+	slog.LogAttrs(ld.ctx, ld.level, ld.msg, ld.attrs...)
+}
+
+func (ld *logData) addAttr(attr slog.Attr) {
+	ld.attrs = append(ld.attrs, attr)
+}
+
+func (dcs *debugClientStream) log(level slog.Level, msg string) *logData {
+	return &logData{
+		level: level,
+		msg:   msg,
+		attrs: []slog.Attr{
+			slog.String("DebugID", dcs.id),
+			slog.String("Method", dcs.method),
+		},
+	}
 }
 
 func (dcs *debugClientStream) Header() (metadata.MD, error) {
-	slog.Info("called Header")
-	return dcs.real.Header()
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "Header"))
+	resp, err := dcs.real.Header()
+	if err != nil {
+		log.addAttr(slog.Any("Error", err))
+	}
+	return resp, err
 }
 
 func (dcs *debugClientStream) Trailer() metadata.MD {
-	slog.Info("called Trailer")
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "Trailer"))
 	return dcs.real.Trailer()
 }
 
 func (dcs *debugClientStream) CloseSend() error {
-	slog.Info("called CloseSend")
-	return dcs.real.CloseSend()
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "CloseSend"))
+	err := dcs.real.CloseSend()
+	if err != nil {
+		log.addAttr(slog.Any("Error", err))
+	}
+	return err
 }
 
 func (dcs *debugClientStream) Context() context.Context {
-	slog.Info("called Context")
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "Context"))
 	return dcs.real.Context()
 }
 
 func (dcs *debugClientStream) SendMsg(m interface{}) error {
-	slog.Info("called SendMsg")
-	return dcs.real.SendMsg(m)
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "SendMsg"))
+	log.addAttr(slog.Int64("MessageSize", int64(unsafe.Sizeof(m))))
+	err := dcs.real.SendMsg(m)
+	if err != nil {
+		log.addAttr(slog.Any("Error", err))
+	}
+	return err
 }
 
 func (dcs *debugClientStream) RecvMsg(m interface{}) error {
-	slog.Info("called RecvMsg")
-	return dcs.real.RecvMsg(m)
+	log := dcs.log(slog.LevelInfo, "ClientStream event")
+	defer log.log()
+	log.addAttr(slog.String(clientStreamMethodName, "RecvMsg"))
+	log.addAttr(slog.Int64("MessageSize", int64(unsafe.Sizeof(m))))
+	err := dcs.real.RecvMsg(m)
+	if err != nil {
+		log.addAttr(slog.Any("Error", err))
+	}
+	return err
 }
